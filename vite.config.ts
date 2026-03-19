61a8f4829b8890805b2c57bada379bbfbffcbe8e
Alan Gloria <102493628+alandavid11@users.noreply.github.com>
2026-03-18 19:50:10 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-  base: '/TripleA/',
+  base: './',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
